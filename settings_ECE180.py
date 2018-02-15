554adffc3d4e5390e3c1c37528bb96fdb9421163
a1amador <36138286+a1amador@users.noreply.github.com>
2018-02-14 18:04:18 -0800

    Update settings_ECE180.py

--- settings_ECE180.py
+++ settings_ECE180.py
@@ -2,5 +2,6 @@
 
 #########  Local output directory for monthly Active Fires datasets in CSV #########
 def get_local_output_directory_M():
-    outdir = "/Users/andre/PycharmProjects/ECE180/Final_Project/MOD14A1_M_FIRE/"
+    # outdir = "/Users/andre/PycharmProjects/ECE180/Final_Project/MOD14A1_M_FIRE/"
+    outdir = "your/local/output/directory"
     return outdir
