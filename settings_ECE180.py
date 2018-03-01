e62a1b1cc785c42b9cea409e298a7000d214f6ad
a1amador <36138286+a1amador@users.noreply.github.com>
2018-03-01 13:04:37 -0800

    Update settings_ECE180.py

--- settings_ECE180.py
+++ settings_ECE180.py
@@ -1,4 +1,7 @@
 # settings_ECE180.py
-
-GOOGLE_API_KEY = 'AIzaSyDdddNuCeI0Y8VouY09cRNQOKEILd9sfE4'
 data_path = './data/'
+# Most operations on Google Maps require that you tell Google who you are. 
+# To authenticate with Google Maps, follow the instructions for creating an API key here:
+# https://console.developers.google.com/flows/enableapi?apiid=maps_backend,geocoding_backend,directions_backend,distance_matrix_backend,elevation_backend&keyType=CLIENT_SIDE&reusekey=true
+GOOGLE_API_KEY = 'your google API key'
+
